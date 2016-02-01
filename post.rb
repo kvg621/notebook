ced0a951971b00e518849b80a0e9f64182f55c5f
kvg621 <konst000@yandex.ru>
2016-02-01 14:31:44 +0300

    notepad V1.0 working

--- post.rb
+++ post.rb
@@ -1,4 +1,11 @@
 class Post
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+  return post_types[type_index].new
+  end
 
   def initialize
     @created_at=Time.now
@@ -15,12 +22,12 @@ class Post
 
   def file_path
     current_path=File.dirname(__FILE__)
-    file_name=@created_at.strftime("#{self.class.name}") _%Y-%m-%d_%H-%M-%S.txt
+    file_name=@created_at.strftime("#{self.class.name} _%Y-%m-%d_%H-%M-%S.txt")
     return current_path+"/"+file_name
   end
 
   def save
-    file=File.new(file_path, w)
+    file=File.new(file_path, "w")
     for item in to_strings do
       file.puts(item)
     end
